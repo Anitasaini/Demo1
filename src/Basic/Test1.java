d4d376c9ecd5d0bd6168b6ab32e128a720469df5
Lenovo <Lenovo@LAPTOP-LLF5TVHN>
2022-11-24 14:51:29 +0530

    First commit:-1

---
+++ src/Basic/Test1.java
@@ -0,0 +1,26 @@
+package Basic;
+
+public class Test1 
+{
+          
+          
+            	public static void main(String[] args)
+      	{	   
+            		String studentname;
+            		 studentname= "Maggi";  
+            		   
+            		   System.out.println(studentname);
+            		   
+            		   m1();
+            		   
+      }
+            	
+       public static void m1() 
+       {
+    	 System.out.println("Anu");  
+    	   
+       }
+            	
+                    	
+            	
+ }
